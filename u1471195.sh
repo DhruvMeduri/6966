43f2bcbf2b514e20e73ebf7ce86919b918c5ab51
Dhruv Meduri <dhruvm@Dhruvs-MacBook-Air.local>
2023-08-23 22:49:15 -0600

    fourth commit

--- u1471195.sh
+++ u1471195.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:a100:1
 #SBATCH --time=8:00:00
 #SBATCH --mem=40GB
-#SBATCH --mail-user=u1471196@umail.utah.edu
+#SBATCH --mail-user=u1471195@umail.utah.edu
 #SBATCH --mail-type=FAIL,END
 #SBATCH -o assignment_1-%j
 
@@ -14,8 +14,8 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate CS6966
 
 mkdir -p /scratch/general/vast/u1471195/huggingface_cache
-export TRANSFORMER_CACHE="/scratch/general/vast/u1471195/huggingface_cache"
+export TRANSFORMERS_CACHE="/scratch/general/vast/u1471195/huggingface_cache"
 
 OUT_DIR=/scratch/general/vast/u1471195/cs6966/assignment1/models
 mkdir -p ${OUT_DIR}
-python assignment1.py --output_dir ${OUT_DIR}
+python ~/6966/assignment1.py --output_dir ${OUT_DIR}
